caaabc4322c19fe10f2c7c7d05c8ec8baf01d87a
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-23 01:11:13 -0300

    added baseline

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./pages/App.jsx";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
