6feb24634776239814f82aa87e7621e02df84b90
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-23 01:37:43 -0300

    added header component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Header from "../components/Header";
+// import SideBar from "../components/SideBar";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "100vh",
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Header />
+      {/* <SideBar /> */}
+    </div>
+  );
+}
