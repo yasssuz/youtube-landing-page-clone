d7483797686e5b4c668f5d85b9378da40b0fa61d
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-23 12:08:10 -0300

    desktop ready

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,11 +6,22 @@ import {
   ListItemText,
   Divider,
   ListItemIcon,
+  Typography,
+  Box,
+  Button,
+  ListSubheader,
 } from "@material-ui/core";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import HomeIcon from "@material-ui/icons/Home";
 import ExploreIcon from "@material-ui/icons/Explore";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import HistoryIcon from "@material-ui/icons/History";
+import SlideshowIcon from "@material-ui/icons/Slideshow";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import AddCircle from "@material-ui/icons/AddCircle";
 
 const useStyles = makeStyles(theme => ({
   drawer: {
@@ -23,6 +34,10 @@ const useStyles = makeStyles(theme => ({
   },
   icons: {
     marginRight: theme.spacing(1),
+  },
+  listItem: {
+    paddingTop: 6,
+    paddingBottom: 6,
   },
 }));
 
@@ -40,7 +55,7 @@ export default function SideBar() {
       <Toolbar />
       <div className={classes.drawerContainer}>
         <List>
-          <ListItem button>
+          <ListItem button classes={{ root: classes.listItem }}>
             <ListItemIcon>
               <HomeIcon />
             </ListItemIcon>
@@ -51,7 +66,7 @@ export default function SideBar() {
               primary='Start'
             />
           </ListItem>
-          <ListItem button>
+          <ListItem button classes={{ root: classes.listItem }}>
             <ListItemIcon>
               <ExploreIcon />
             </ListItemIcon>
@@ -59,10 +74,10 @@ export default function SideBar() {
               classes={{
                 primary: classes.listItemText,
               }}
-              primary='Higher Up'
+              primary='Explore'
             />
           </ListItem>
-          <ListItem button>
+          <ListItem button classes={{ root: classes.listItem }}>
             <ListItemIcon>
               <SubscriptionsIcon />
             </ListItemIcon>
@@ -75,6 +90,181 @@ export default function SideBar() {
           </ListItem>
         </List>
         <Divider />
+        <List>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <VideoLibraryIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='Library'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <HistoryIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='History'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <SlideshowIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='Your videos'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <WatchLaterIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='Watch later'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <ThumbUpIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='Videos you liked'
+            />
+          </ListItem>
+        </List>
+        <Divider />
+        <Box p={4}>
+          <Typography variant='body2'>
+            Login your account to like, comment and subscribe.
+          </Typography>
+          <Box mt={4} display='flex' flexDirection='column'>
+            <Button
+              variant='outlined'
+              color='secondary'
+              startIcon={<AccountCircleIcon />}
+            >
+              Login
+            </Button>
+          </Box>
+        </Box>
+        <Divider />
+        <List
+          component='nav'
+          aria-labelledby='nested-list-subheader'
+          subheader={
+            <ListSubheader
+              component='div'
+              id='nested-list-subheader'
+              className={classes.subheader}
+            >
+              The best of Youtube
+            </ListSubheader>
+          }
+        >
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='Music'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='Sports'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='Games'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='Movies'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='News'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='Live'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='Outstanding'
+            />
+          </ListItem>
+          <ListItem button classes={{ root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText
+              classes={{
+                primary: classes.listItemText,
+              }}
+              primary='360deg videos'
+            />
+          </ListItem>
+        </List>
       </div>
     </Drawer>
   );
