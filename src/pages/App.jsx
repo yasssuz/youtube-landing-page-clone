930f9fde05f3011a725a04d43f75dc36b203c45b
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-23 01:13:30 -0300

    added theme

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,8 +1,23 @@
+import Home from "./Home";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
 function App() {
+  const theme = createMuiTheme({
+    spacing: 4,
+    palette: {
+      primary: {
+        main: "#f44336",
+      },
+      secondary: {
+        main: "#3F51B5",
+      },
+    },
+  });
+
   return (
-    <div className='App'>
-      <h1>hello</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
